68afc309577397799357bac83554f21fa33df26e
FabianMamani <fabian.mamani@hotmail.com>
2024-10-18 15:23:25 -0300

    add sendMail

--- src/main/java/notificadores/Telegram.java
+++ src/main/java/notificadores/Telegram.java
@@ -3,19 +3,44 @@ package notificadores;
 import classroom.notifier.entity.Alumno;
 import classroom.notifier.implement.MedioComunicacion;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 
 public class Telegram implements MedioComunicacion {
     String name = "Telegram";
+    private static final String BOT_TOKEN = "7834829611:AAHBM2XOqC0F5dYFFqVlxENXNlgLYMq032w";
+    private static final String CHAT_ID = "953342229";
     @Override
     public void Notificar(String Materia, String Aula, List<Alumno> destinatarios) {
-        String msj = "La materia: "+ Materia+" cambio a Aula"+ Aula;
-        System.out.println(msj);
-        System.out.println("Destinatarios" + destinatarios.toString());
+        String msj = "La materia: "+ Materia+" cambio a Aula -> "+ Aula;
+        try {
+            sendMessage(msj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void sendMessage(String message) throws Exception {
+        String urlString = "https://api.telegram.org/bot" + BOT_TOKEN + "/sendMessage?chat_id=" + CHAT_ID + "&text=" + message;
+        URL url = new URL(urlString);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+
+        // Leer la respuesta
+        int responseCode = conn.getResponseCode();
+        if (responseCode == 200) {
+            System.out.println("Mensaje enviado exitosamente.");
+        } else {
+            System.out.println("Error al enviar el mensaje: " + responseCode);
+        }
+        conn.disconnect();
     }
 
     @Override
     public String getMedio() {
         return name;
     }
+
+    //953342229
+
 }
--- src/test/java/NotificadorTest.java
+++ src/test/java/NotificadorTest.java
@@ -1,5 +1,6 @@
 
 import classroom.notifier.implement.MedioComunicacion;
+import notificadores.Telegram;
 import notificadores.Whatsapp;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -9,8 +10,8 @@ public class NotificadorTest {
     public MedioComunicacion notificador;
     @Test
     public void WhatsappTest(){
-        notificador = new Whatsapp();
-        Assertions.assertEquals(notificador.getMedio(), "Whatsapp");
+        notificador = new Telegram();
+        notificador.Notificar("PP2","A0102", null);
     }
 
 
